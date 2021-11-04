c14d4a05623d5e19b3ec7040566e46fb55d8f9e4
Yousung Jung <jys01012@gmail.com>
2021-11-04 16:29:16 +0900

    ubuntu 공장초기화

---
+++ os/linux/debian/factory_reset.sh
@@ -0,0 +1,11 @@
+# !bin/sh
+
+sudo dpkg --configure -a
+sudo apt-get update
+sudo apt-get -f install
+sudo apt-get full-upgrade
+sudo apt-get install --reinstall ubuntu-desktop
+sudo apt-get autoremove
+
+sudo apt-get clean
+sudo reboot
